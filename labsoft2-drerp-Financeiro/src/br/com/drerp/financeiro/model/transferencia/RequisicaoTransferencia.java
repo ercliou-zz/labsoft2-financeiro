16fb4dd93286840e137d176fd2fea7799ee486cd
helderppaulo89@gmail.com <helderppaulo89@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-14 00:19:35 +0000

    helder

--- labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/RequisicaoTransferencia.java
+++ labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/RequisicaoTransferencia.java
@@ -1,5 +1,6 @@
 package br.com.drerp.financeiro.model.transferencia;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -9,69 +10,100 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="FIN_REQUISICAO")
+@Table(name = "FIN_REQUISICAO")
 public class RequisicaoTransferencia {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
+	
 	@OneToOne
 	private Beneficiario beneficiario;
+	
+	private BigDecimal valor;
+	
 	@OneToOne
 	private Pagador pagador;
+	
 	private StatusRequisicaoTransferencia status;
+	
 	private Long dataRequisicaoMilis;
+	
 	private Long dataLimiteMilis;
+	
 	@OneToOne(mappedBy = "requisicao")
 	private Transferencia transferencia;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Beneficiario getBeneficiario() {
 		return beneficiario;
 	}
+
 	public void setBeneficiario(Beneficiario beneficiario) {
 		this.beneficiario = beneficiario;
 	}
+
 	public Pagador getPagador() {
 		return pagador;
 	}
+
 	public void setPagador(Pagador pagador) {
 		this.pagador = pagador;
 	}
+
 	public StatusRequisicaoTransferencia getStatus() {
 		return status;
 	}
+
 	public void setStatus(StatusRequisicaoTransferencia status) {
 		this.status = status;
 	}
+
 	public Long getDataRequisicaoMilis() {
 		return dataRequisicaoMilis;
 	}
+
 	public void setDataRequisicaoMilis(Long dataRequisicaoMilis) {
 		this.dataRequisicaoMilis = dataRequisicaoMilis;
 	}
-	public Date getDataRequisicao(){
+
+	public Date getDataRequisicao() {
 		return new Date(this.dataRequisicaoMilis);
 	}
+
 	public Long getDataLimiteMilis() {
 		return dataLimiteMilis;
 	}
+
 	public void setDataLimiteMilis(Long dataLimiteMilis) {
 		this.dataLimiteMilis = dataLimiteMilis;
 	}
-	public Date getDataLimite(){
+
+	public Date getDataLimite() {
 		return new Date(this.dataLimiteMilis);
 	}
+
 	public Transferencia getTransferencia() {
 		return transferencia;
 	}
+
 	public void setTransferencia(Transferencia transferencia) {
 		this.transferencia = transferencia;
 	}
-	
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
 }
