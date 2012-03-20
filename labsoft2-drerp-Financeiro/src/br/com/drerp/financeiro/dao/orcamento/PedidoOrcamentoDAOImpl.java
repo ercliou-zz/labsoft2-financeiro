6f7b40133727da4f76f860c134831864e058956b
ercliou@gmail.com <ercliou@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-20 22:52:25 +0000

    PedidoOrcamento

---
+++ labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/dao/orcamento/PedidoOrcamentoDAOImpl.java
@@ -0,0 +1,8 @@
+package br.com.drerp.financeiro.dao.orcamento;
+
+import br.com.drerp.financeiro.dao.GenericDAOImpl;
+import br.com.drerp.financeiro.model.orcamento.PedidoOrcamento;
+
+public class PedidoOrcamentoDAOImpl extends GenericDAOImpl<PedidoOrcamento> implements PedidoOrcamentoDAO {
+
+}
