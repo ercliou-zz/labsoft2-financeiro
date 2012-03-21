f158949298128b90792a38fc6f027948afbde9dd
nakamura.tiago@gmail.com <nakamura.tiago@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-21 04:32:40 +0000

    Implementado model dos tributos

---
+++ trunk/labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/tributo/EmprestimoCompulsorio.java
@@ -0,0 +1,25 @@
+package br.com.drerp.financeiro.model.transferencia.tributo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import br.com.drerp.financeiro.dao.GenericModel;
+
+@Entity
+@Table(name = "FIN_EMPRESTIMO_COMPULSORIO")
+public class EmprestimoCompulsorio extends GenericModel {
+
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, targetEntity = Tributo.class)
+	private Tributo tributo;
+
+	public Tributo getTributo() {
+		return tributo;
+	}
+
+	public void setTributo(Tributo tributo) {
+		this.tributo = tributo;
+	}
+
+}
