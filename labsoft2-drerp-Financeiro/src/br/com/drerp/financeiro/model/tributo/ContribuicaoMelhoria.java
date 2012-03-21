77dc58bf50bf63934e84c2ace1da6e01fbd193b2
nakamura.tiago@gmail.com <nakamura.tiago@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-21 13:11:27 +0000

    realocado package de tributos

---
+++ labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/tributo/ContribuicaoMelhoria.java
@@ -0,0 +1,23 @@
+package br.com.drerp.financeiro.model.tributo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FIN_CONTRIBUICAO_MELHORIA")
+public class ContribuicaoMelhoria {
+	
+	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, targetEntity = Tributo.class)
+	private Tributo tributo;
+
+	public Tributo getTributo() {
+		return tributo;
+	}
+
+	public void setTributo(Tributo tributo) {
+		this.tributo = tributo;
+	}
+
+}
