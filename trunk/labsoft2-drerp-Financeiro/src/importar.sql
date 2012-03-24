5ecf821621bbc0ce7e5d8a961a29702944e58589
ercliou@gmail.com <ercliou@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-24 03:22:43 +0000

    Graficos Orcamento

--- trunk/labsoft2-drerp-Financeiro/src/importar.sql
+++ trunk/labsoft2-drerp-Financeiro/src/importar.sql
@@ -26,9 +26,22 @@ INSERT INTO `financeiro-db`.`fin_planosaude` (`id`, `nome`) VALUES (7, 'Murilo')
 
 
 -- PEDIDOORCAMENTO
-INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (1, 1, 1);
-INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (2, 2, 2);
-INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (3, 3, 3);
---INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (4, 1, 1);
---INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (5, 1, 1);
---INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `pagador_id`, `plano_id`) VALUES (6, 2, 2);
+--Dec 13 2011
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (1, 1323741600000, 1, 1);
+--Jan 21 2012
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (2, 1327111200000, 2, 2);
+--Feb 21 2012
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (3, 1329789600000, 3, 3);
+--Dez 14 2011
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (4, 1323828000000, 1, 1);
+--March 14 2012
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (5, 1331694000000, 1, 1);
+INSERT INTO `financeiro-db`.`fin_pedidoorcamento` (`id`, `dataPedidoMilis`, `pagador_id`, `plano_id`) VALUES (6, 1331694000000, 1, 1);
+
+--ORCAMENTO
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (1, 100.11, 1);
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (2, 200.22, 2);
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (3, 300.33, 3);
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (4, 400.44, 4);
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (5, 500.55, 5);
+INSERT INTO `financeiro-db`.`fin_orcamento` (`id`, `valorFinal`, `pedido_fk`) VALUES (6, 600.66, 6);
