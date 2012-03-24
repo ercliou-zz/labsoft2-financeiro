433056950f9acc79f5a3593bc37cd75c3df4a27b
ercliou@gmail.com <ercliou@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-24 03:22:22 +0000

    Graficos Orcamento

--- trunk/labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/dao/orcamento/PedidoOrcamentoDAOImpl.java
+++ trunk/labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/dao/orcamento/PedidoOrcamentoDAOImpl.java
@@ -6,14 +6,15 @@ import org.hibernate.criterion.Restrictions;
 
 import br.com.drerp.financeiro.dao.GenericDAOImpl;
 import br.com.drerp.financeiro.model.orcamento.PedidoOrcamento;
-import br.com.drerp.financeiro.model.transferencia.RequisicaoTransferencia;
-import br.com.drerp.financeiro.model.transferencia.StatusRequisicaoTransferencia;
+import br.com.drerp.financeiro.model.planosaude.PlanoSaude;
 
 public class PedidoOrcamentoDAOImpl extends GenericDAOImpl<PedidoOrcamento> implements PedidoOrcamentoDAO {
 	
 	@SuppressWarnings("unchecked")
-	public List<RequisicaoTransferencia> listByDataPedido(Long inicio, Long fim) {
-		return session.createCriteria(RequisicaoTransferencia.class).add(Restrictions.between("dataPedidoMilis", inicio, fim)).list();
+	public List<PedidoOrcamento> listByDataPedido(PlanoSaude plano, Long inicio, Long fim) { // DATAREQUISICAOMILIS
+		return session.createCriteria(PedidoOrcamento.class).
+				add(Restrictions.between("dataPedidoMilis", inicio, fim)).
+				add(Restrictions.eq("plano", plano)).list();
 	}
 	
 
