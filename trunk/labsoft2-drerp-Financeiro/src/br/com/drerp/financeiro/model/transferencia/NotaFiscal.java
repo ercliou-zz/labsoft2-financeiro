eab91094d943acc68202f4e167da76a6e5212447
nakamura.tiago@gmail.com <nakamura.tiago@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-28 04:21:11 +0000

    implementado CRUD pagador e beneficiario

--- trunk/labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/NotaFiscal.java
+++ trunk/labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/NotaFiscal.java
@@ -1,5 +1,8 @@
 package br.com.drerp.financeiro.model.transferencia;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -8,12 +11,24 @@ import javax.persistence.Table;
 import br.com.drerp.financeiro.model.GenericModel;
 
 @Entity
-@Table(name="FIN_NOTAFISCAL")
-public class NotaFiscal extends GenericModel{
-	
+@Table(name = "FIN_NOTAFISCAL")
+public class NotaFiscal extends GenericModel {
+
 	@OneToOne
-	@JoinColumn(name="contaReceber_fk")
+	@JoinColumn(name = "contaReceber_fk")
 	private ContaReceber contaReceber;
+
+	private Integer aliquotaIcms;
+	private Integer aliquotaIss;
+	private Integer aliquotaIpi;
+
+	private BigDecimal icms;
+	private BigDecimal iss;
+	private BigDecimal ipi;
+
+	private BigDecimal valorFinal;
+
+	private Date dataEmissao;
 
 	public ContaReceber getContaReceber() {
 		return contaReceber;
@@ -22,5 +37,69 @@ public class NotaFiscal extends GenericModel{
 	public void setContaReceber(ContaReceber contaReceber) {
 		this.contaReceber = contaReceber;
 	}
-	
+
+	public Integer getAliquotaIcms() {
+		return aliquotaIcms;
+	}
+
+	public void setAliquotaIcms(Integer aliquotaIcms) {
+		this.aliquotaIcms = aliquotaIcms;
+	}
+
+	public Integer getAliquotaIss() {
+		return aliquotaIss;
+	}
+
+	public void setAliquotaIss(Integer aliquotaIss) {
+		this.aliquotaIss = aliquotaIss;
+	}
+
+	public Integer getAliquotaIpi() {
+		return aliquotaIpi;
+	}
+
+	public void setAliquotaIpi(Integer aliquotaIpi) {
+		this.aliquotaIpi = aliquotaIpi;
+	}
+
+	public BigDecimal getIcms() {
+		return icms;
+	}
+
+	public void setIcms(BigDecimal icms) {
+		this.icms = icms;
+	}
+
+	public BigDecimal getIss() {
+		return iss;
+	}
+
+	public void setIss(BigDecimal iss) {
+		this.iss = iss;
+	}
+
+	public BigDecimal getIpi() {
+		return ipi;
+	}
+
+	public void setIpi(BigDecimal ipi) {
+		this.ipi = ipi;
+	}
+
+	public BigDecimal getValorFinal() {
+		return valorFinal;
+	}
+
+	public void setValorFinal(BigDecimal valorFinal) {
+		this.valorFinal = valorFinal;
+	}
+
+	public Date getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public void setDataEmissao(Date dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+
 }
