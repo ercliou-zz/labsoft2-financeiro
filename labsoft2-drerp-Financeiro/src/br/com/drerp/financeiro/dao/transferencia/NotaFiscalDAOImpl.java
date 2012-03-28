95880f002d34809412b64939e327ce31d606b515
nakamura.tiago@gmail.com <nakamura.tiago@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-03-28 12:47:01 +0000

    Implementado resto do CRUD da nota fiscal

---
+++ labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/dao/transferencia/NotaFiscalDAOImpl.java
@@ -0,0 +1,8 @@
+package br.com.drerp.financeiro.dao.transferencia;
+
+import br.com.drerp.financeiro.dao.GenericDAOImpl;
+import br.com.drerp.financeiro.model.transferencia.NotaFiscal;
+
+public class NotaFiscalDAOImpl extends GenericDAOImpl<NotaFiscal> implements NotaFiscalDAO {
+
+}
