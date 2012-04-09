c0a01634eda34cdfaedd3670ff829cdcd9700fbd
ercliou@gmail.com <ercliou@gmail.com@f46b5ea4-0ad3-2981-750c-88d1d99533b8>
2012-04-09 19:56:22 +0000

    Model ContaRec

--- labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/ContaReceber.java
+++ labsoft2-drerp-Financeiro/src/br/com/drerp/financeiro/model/transferencia/ContaReceber.java
@@ -1,8 +1,13 @@
 package br.com.drerp.financeiro.model.transferencia;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -11,6 +16,10 @@ import javax.persistence.Table;
 public class ContaReceber extends Transferencia implements Serializable {
 	
 	private static final long serialVersionUID = 2306774825117902576L;
+	
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinColumn(name="contaReceber_id")
+	private List<Fatura> faturas;
 	
 	@OneToOne
 	private NotaFiscal notaFiscal;
@@ -22,5 +31,14 @@ public class ContaReceber extends Transferencia implements Serializable {
 	public void setNotaFiscal(NotaFiscal notaFiscal) {
 		this.notaFiscal = notaFiscal;
 	}
+
+	public List<Fatura> getFaturas() {
+		return faturas;
+	}
+
+	public void setFaturas(List<Fatura> faturas) {
+		this.faturas = faturas;
+	}
+	
 	
 }
